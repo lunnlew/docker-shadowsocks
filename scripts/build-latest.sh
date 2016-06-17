3bbe21407b6b36215ddfd309ecf95c6ff2c694c4
lunnlew <lunnlew@qq.com>
2016-06-17 14:24:47 +0800

    update

--- scripts/build-latest.sh
+++ scripts/build-latest.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 path_compose=~/docker-compose
 $path_compose up --build -d latest
+
+#docker run --name ss -p 8388:8388/tcp -p 8388:8388/udp -e PASSWORD=5ouMnqPyzseL -e  METHOD=aes-256-cfb dockershadowsocks_latest
