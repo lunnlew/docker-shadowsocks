4c17ae001a5369a3194f6a43adef3375437dfeb4
lunnlew <lunnlew@qq.com>
2016-06-17 14:49:37 +0800

    update

--- scripts/build-latest.sh
+++ scripts/build-latest.sh
@@ -2,4 +2,4 @@
 path_compose=~/docker-compose
 $path_compose up --build -d latest
 
-#docker run --name ss -p 8388:8388/tcp -p 8388:8388/udp -e PASSWORD=5ouMnqPyzseL -e  METHOD=aes-256-cfb dockershadowsocks_latest
+#docker run --name ss -p 8388:8388/tcp -p 8388:8388/udp -e PASSWORD=5ouMnqPyzseL -e  METHOD=aes-256-cfb --restart always dockershadowsocks_latest
---
+++ scripts/deploy-latest.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker commit $(docker ps -a -q) lunnlew/shadowsocks-server
+docker push lunnlew/shadowsocks-server
